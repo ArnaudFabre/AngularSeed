f74ee96094c837b6737f523839f79b751899c09e
Arnaud <arnaud.fabre@gmail.com>
2015-12-31 16:56:29 +0100

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # AngularSeed
-Basic Angular App bash seed
+
+Basic bash script to create simple demonstration Angular App with :
+* one empty css in assets directory
+* one index.html file with one controller
+* one app.js file with one controller
+
+Add one argument : the name of the App
+
+You maybe want to install first npm (plenty of tutorials on the web), bower, and angular :
+* npm install -g bower
+* bower init
+* bower install -S angular
---
+++ angular_seed.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir assets
+
+touch assets/style.css
+touch index.html
+touch app.js
+
+echo "<!doctype html>" > index.html
+echo "<html lang='en' ng-app='${1}App'>" >> index.html
+echo "  <header>" >> index.html
+echo "      <meta charset='utf-8'>" >> index.html
+echo "      <title>${1}App</title>" >> index.html
+echo "      <link rel='stylesheet' href='assets/style.css'>" >> index.html
+echo "      <script src='bower_components/angular/angular.js'></script>" >> index.html
+echo "      <script src='app.js'></script>" >> index.html
+echo "  </header>" >> index.html
+echo "  <body ng-congtroller='${1}Ctrl'>" >> index.html
+echo "  </body>" >> index.html
+echo "</html>" >> index.html
+
+echo "var ${1}App = angular.module('${1}App', []);" > app.js
+echo "" >> app.js
+echo "${1}App.controller('${1}Ctrl', function (\$scope) {" >> app.js
+echo "" >> app.js
+echo "});" >> app.js
+
