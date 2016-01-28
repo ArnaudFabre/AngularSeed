83c5c6f30c31b76fb22a0413e55d08774d62da55
arnaud.fabre <arnaud.fabre@gmail.com>
2016-01-28 11:27:56 +0100

    header typo

--- angular_seed.sh
+++ angular_seed.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+/bin/bash: eader : commande introuvable
 
 mkdir assets
 
@@ -8,13 +8,13 @@ touch app.js
 
 echo "<!doctype html>" > index.html
 echo "<html lang='en' ng-app='${1}App'>" >> index.html
-echo "  <header>" >> index.html
+echo "  <head>" >> index.html
 echo "      <meta charset='utf-8'>" >> index.html
 echo "      <title>${1}App</title>" >> index.html
 echo "      <link rel='stylesheet' href='assets/style.css'>" >> index.html
 echo "      <script src='bower_components/angular/angular.js'></script>" >> index.html
 echo "      <script src='app.js'></script>" >> index.html
-echo "  </header>" >> index.html
+echo "  </head>" >> index.html
 echo "  <body ng-controller='${1}Ctrl'>" >> index.html
 echo "  </body>" >> index.html
 echo "</html>" >> index.html
