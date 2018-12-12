dc7b118d8f07942a495d5bd67d850a9fac7684b8
Arnaud Fabre <arnaud.fabre@caldera.com>
2018-12-12 15:10:28 +0100

    Support Yarn by default now

--- README.md
+++ README.md
@@ -5,13 +5,25 @@ Basic bash script to create simple demonstration Angular App with :
 * one index.html file with one controller
 * one app.js file with one controller
 
-Add one argument : the name of the App
 ```
-./angular_seed.sh MyNewApp
+./angular_seed.sh MyNewApp [bower|yarn]
 ```
+
+## Install Angular
+
+### Bower
+
+__Deprecated__
 
 You just have then to install bower, and angular :
 ```
 sudo npm install -g bower
-bower init && bower install -S angular"
+bower init && bower install -S angular
+```
+### Yarn
+
+You just have then to install yarn, and angular :
+```
+sudo npm install -g yarn
+yarn init && yarn install angular --dev
 ```
--- angular_seed.sh
+++ angular_seed.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
-then
-	echo "./angular_seed seed_name"
+PKG="yarn"
+DIR="node_modules"
+CMD="yarn add angular --dev"
+
+case $# in
+1)
+	echo "Generating $1..."
+;;
+2)
+	case $2 in
+		"yarn")
+			PKG="yarn"
+			DIR="node_modules"
+			CMD="yarn add angular --dev"
+		;;
+		"bower")
+			PKG="bower"
+			DIR="bower_components"
+			CMD="bower install -S angular"
+		;;
+		*)
+		 echo "Package manager $2 not supported"
+		 exit 2
+	esac
+	echo "Generating $1 with $2..."
+;;
+*)
+	echo "./angular_seed seed_name [bower|yarn]"
 	exit 1
-fi
+;;
+esac
 
 mkdir $1
 cd $1
@@ -20,7 +46,7 @@ echo "  <head>" >> index.html
 echo "      <meta charset='utf-8'>" >> index.html
 echo "      <title>${1}App</title>" >> index.html
 echo "      <link rel='stylesheet' href='assets/style.css'>" >> index.html
-echo "      <script src='bower_components/angular/angular.js'></script>" >> index.html
+echo "      <script src='$DIR/angular/angular.js'></script>" >> index.html
 echo "      <script src='app.js'></script>" >> index.html
 echo "  </head>" >> index.html
 echo "  <body ng-controller='${1}Ctrl'>" >> index.html
@@ -34,5 +60,5 @@ echo "" >> app.js
 echo "});" >> app.js
 
 echo "Now run the following command:"
-echo "sudo npm install -g bower"
-echo "cd $1  && bower init && bower install -S angular"
+echo "sudo npm install -g $PKG"
+echo "cd $1  && $PKG init && $CMD"
